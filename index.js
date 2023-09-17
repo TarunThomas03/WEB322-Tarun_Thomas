bc6e2b62a52cea68d683f5a2f119fc0cab764fe2
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2023-09-17 15:57:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const { EventEmitter } = require("events");
 
 const trafficLightsEventEmitter = new EventEmitter();
 
+// on / emit
+
+// Structuring data
 const lights = [
   { color: "red", duration: 5000 },
   { color: "yellow", duration: 2000 },
   { color: "green", duration: 5000 },
-];
+]
 
-let colorIndex = 0;
+let colorIdx = 0;
 let counter = 3;
 
 // Color Change
 function changeColor() {
-  const currentColor = lights[colorIndex].color;
+  const currentColor = lights[colorIdx].color;
   console.log(currentColor);
   trafficLightsEventEmitter.emit("colorChange", currentColor);
-  colorIndex = (colorIndex + 1) % lights.length;
+  colorIdx = (colorIdx + 1) % lights.length;
 }
 
 const interval = setInterval(changeColor, 5000);
