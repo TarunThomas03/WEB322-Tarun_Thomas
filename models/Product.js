1abe1d272a0633ae934584377bc2cd5f98a7b991
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2023-11-30 22:32:01 -0500

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  isbn: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
