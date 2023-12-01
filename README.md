452725e2a0e71087da3317fb958fbcc40585bc67
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2023-11-30 22:37:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Clone the repository. Then run `npm install`.`npm start`. Visit `http://localhost:3000` in your browser.
-Will update more briefly once pushed.
+Clone the repository:
+
+Navigate to the project folder:
+
+Install dependencies:
+
+`npm install`
+
+To run the project locally, use:
+
+`npm start`
+`node server.js`
+Access the server at: `http://localhost:3000/`
--- server.js
+++ server.js
@@ -1,51 +1,57 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const users = require('./models/users.json');
+const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT from environment or default to 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
+
+// Import API router
 const apiRouter = require('./routes/api');
-
 app.use('/api', apiRouter);
 
 // SET THE VIEW ENGINE
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 // ROUTE HANDLING
-app.get('/', (req, res) => {
-    res.render('login', { error: req.query.error });
+app.get("/", (req, res) => {
+  res.render('login', { error: req.query.error });
 });
 
-app.post('/', (req, res) => {
-    res.redirect(`/list`);
+app.post("/", (req, res) => {
+  res.redirect(`/list`);
 });
 
-app.get('/list', (req, res) => {
-    const itemsToDisplay = 15;
-    const page = parseInt(req.query?.page) || 1;
-    const start = page === 1 ? 0 : (page - 1) * itemsToDisplay - 1;
-    const end = start + itemsToDisplay;
-    const paginatedUsers = users.slice(start, end);
+app.get("/list", (req, res) => {
+  const itemsToDisplay = 15;
+  const page = parseInt(req.query?.page) || 1;
+  const start = (page === 1) ? 0 : (page - 1) * itemsToDisplay - 1;
+  const end = start + itemsToDisplay;
+  const filteredUsers = users.slice(start + 1, end + 1); // Adjusted array slicing
 
-    res.render('list', {
-        title: 'list',
-        users: paginatedUsers,
-        itemsToDisplay,
-        page,
-        start,
-        end,
-    });
+  res.render("list", {
+    title: "list",
+    users: filteredUsers,
+    itemsToDisplay,
+    page,
+    start,
+    end,
+  });
 });
 
-app.get('/detail/:id', (req, res) => {
-    const user = users.find((user) => user.id === parseInt(req.params.id));
-    res.render('detail', { user });
+app.get("/detail/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  res.render("detail", { user });
 });
 
-// LISTEN FOR REQUESTS
+// LISTEN FOR REQUESTS!!!!
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
